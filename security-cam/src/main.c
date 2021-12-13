146ff21890475f54167b0180d9d6d714a44bf48e
Roman Janiczek <roman@janiczek.cz>
2021-12-13 16:19:54 +0100

    ESP32 Cam done

--- security-cam/src/main.c
+++ security-cam/src/main.c
@@ -21,9 +21,6 @@
 #include <esp_wifi.h>
 // ============================= HTTP =============================
 #include <esp_http_server.h>
-// ============================ SPIFFS ============================
-#include "esp_vfs.h"
-#include "esp_spiffs.h"
 // ============================ CAMERA ============================
 #include "esp_camera.h"
 // ================================================================
@@ -35,15 +32,12 @@
 #define WIFI_SSID       "ESP32-Cam AP"
 #define WIFI_CHAN       7
 #define WIFI_MSCO       4
-// ============================= HTTP =============================
-#define SCRATCH_BUFSIZE  8192
-struct file_server_data {
-    char base_path[ESP_VFS_PATH_MAX + 1];
-    char scratch[SCRATCH_BUFSIZE];
-};
-// ============================ SPIFFS ============================
-#define MOUNTPOINT      "/spiffs"
 // ============================ CAMERA ============================
+/**
+ * @brief Camera function - take picture and save on SPIFFS
+ */
+esp_err_t take_picture();
+
 #define CAM_PIN_PWDN    32
 #define CAM_PIN_RESET   -1              //software reset will be performed
 #define CAM_PIN_XCLK    0
@@ -91,6 +85,15 @@ static camera_config_t camera_config = {
         .jpeg_quality = 12, //0-63 lower number means higher quality
         .fb_count = 1       //if more than one, i2s runs in continuous mode. Use only with JPEG
     };
+
+/**
+ * Latest photo store 
+ */
+size_t taken_photo_len = 0;
+size_t taken_photo_width = 0;
+size_t taken_photo_height = 0;
+pixformat_t taken_photo_format = PIXFORMAT_JPEG;
+uint8_t *taken_photo_buf = NULL; 
 // ================================================================
 
 
@@ -219,64 +222,40 @@ esp_err_t get_handler(httpd_req_t *req) {
  */
 esp_err_t img_handler(httpd_req_t *req) {
     ESP_LOGI(DEVICE, "[HTTP] GET latest-photo");
-    const char *filepath = "/spiffs/latest-photo.jpg";
-    FILE *fd = NULL;
-    struct stat file_stat;
-
-    if (stat(filepath, &file_stat) == -1) {
-        ESP_LOGE(DEVICE, "[HTTP] GET latest-photo - failed to get file %s", filepath);
-        httpd_resp_send_err(req, HTTPD_404_NOT_FOUND, "File does not exist");
+    if (taken_photo_buf == NULL) {
+        ESP_LOGE(DEVICE, "[HTTP] CMD latest-photo no picture found");
+        httpd_resp_send_err(req, HTTPD_404_NOT_FOUND, "No picture found!");
         return ESP_FAIL;
     }
-
-    fd = fopen(filepath, "r");
-    if (!fd) {
-        ESP_LOGE(DEVICE, "[HTTP] GET latest-photo - failed to read existing file : %s", filepath);
-        httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Failed to read existing file");
-        return ESP_FAIL;
+    ESP_LOGI(DEVICE, "[CAM] Sending photo");
+    esp_err_t res = httpd_resp_set_type(req, "image/jpeg");
+    if(res == ESP_OK){
+        res = httpd_resp_set_hdr(req, "Content-Disposition", "inline; filename=capture.jpg");
     }
 
-    ESP_LOGI(DEVICE, "[HTTP] GET latest-photo - sending file : %s (%ld bytes)...", "latest-photo.jpg", file_stat.st_size);
-    httpd_resp_set_type(req, "image/jpeg");
-
-    char *chunk = ((struct file_server_data *)req->user_ctx)->scratch;
-    size_t chunksize;
-    do {
-        chunksize = fread(chunk, 1, SCRATCH_BUFSIZE, fd);
-
-        if (chunksize > 0) {
-            if (httpd_resp_send_chunk(req, chunk, chunksize) != ESP_OK) {
-                fclose(fd);
-                ESP_LOGE(DEVICE, "[HTTP] GET latest-photo - file sending failed!");
-                httpd_resp_sendstr_chunk(req, NULL);
-                httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Failed to send file");
-               return ESP_FAIL;
-           }
+    if(res == ESP_OK){
+        if(taken_photo_format == PIXFORMAT_JPEG){
+            res = httpd_resp_send(req, (const char *)taken_photo_buf, taken_photo_len);
+        } else {
+            ESP_LOGE(DEVICE, "[HTTP] CMD latest-photo failed to send");
+            httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Failed to send picture!");
+            return ESP_FAIL;
         }
-    } while (chunksize != 0);
-
-    fclose(fd);
-    ESP_LOGI(DEVICE, "[HTTP] GET latest-photo - file sending complete");
-
-    httpd_resp_set_hdr(req, "Connection", "close");
-    httpd_resp_send_chunk(req, NULL, 0);
+    }
     return ESP_OK;
 }
 
 /**
- * @brief Camera function - take picture and save on SPIFFS
+ * @brief Get Handler for Webserver - pir-event - due to problem with POST signal for taking photo implemented as GET
  */
-esp_err_t take_picture();
-/**
- * @brief Get Handler for Webserver - take-photo - due to problem with POST signal for taking photo implemented as GET
- * source: https://github.com/espressif/esp-idf/blob/master/examples/protocols/http_server/file_serving/main/file_server.c
- */
-esp_err_t take_handler(httpd_req_t *req) {
+esp_err_t pir_handler(httpd_req_t *req) {
     ESP_LOGI(DEVICE, "[HTTP] GET take-photo");
     esp_err_t ret = take_picture();
     if (ret == ESP_OK) {
         ESP_LOGI(DEVICE, "[HTTP] CMD take-photo success");
-        httpd_resp_send(req, "Picture taken!", HTTPD_RESP_USE_STRLEN);
+        char msg_buffer[127];
+        sprintf(msg_buffer, "Picture taken! (%zu x %zu, size: %zu bytes)", taken_photo_width, taken_photo_height, taken_photo_len);
+        httpd_resp_send(req, msg_buffer, HTTPD_RESP_USE_STRLEN);
         return ESP_OK;
     } else {
         ESP_LOGI(DEVICE, "[HTTP] CMD take-photo failure");
@@ -286,37 +265,71 @@ esp_err_t take_handler(httpd_req_t *req) {
 }
 
 /**
- * @brief Uri Handler for Webserver
- * source: https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/protocols/esp_http_server.html
+ * @brief For HTTP TAKE PHOTO request
+ * source: https://github.com/espressif/esp32-camera/blob/master/README.md
  */
-httpd_uri_t uri_get = {
-    .uri      = "/",
-    .method   = HTTP_GET,
-    .handler  = get_handler,
-    .user_ctx = NULL
-};
+typedef struct {
+        httpd_req_t *req;
+        size_t len;
+} jpg_chunking_t;
 
 /**
- * @brief Uri Handler for Webserver
- * source: https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/protocols/esp_http_server.html
+ * @brief For HTTP TAKE PHOTO request
+ * source: https://github.com/espressif/esp32-camera/blob/master/README.md
  */
-httpd_uri_t img_get = {
-    .uri      = "/latest-photo.jpg",
-    .method   = HTTP_GET,
-    .handler  = img_handler,
-    .user_ctx = NULL
-};
+static size_t jpg_encode_stream(void * arg, size_t index, const void* data, size_t len){
+    jpg_chunking_t *j = (jpg_chunking_t *)arg;
+    if(!index){
+        j->len = 0;
+    }
+    if(httpd_resp_send_chunk(j->req, (const char *)data, len) != ESP_OK){
+        return 0;
+    }
+    j->len += len;
+    return len;
+}
 
 /**
- * @brief Uri Handler for Webserver
- * source: https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/protocols/esp_http_server.html
+ * @brief Get Handler for Webserver - take-photo - due to problem with POST signal for taking photo implemented as GET
+ * source: https://github.com/espressif/esp32-camera/blob/master/README.md
  */
-httpd_uri_t img_post = {
-    .uri      = "/take-photo",
-    .method   = HTTP_GET,
-    .handler  = take_handler,
-    .user_ctx = NULL
-};
+esp_err_t take_handler(httpd_req_t *req){
+    ESP_LOGI(DEVICE, "[CAM] Taking photo");
+    
+    gpio_set_level(4, 1);
+    vTaskDelay(delay);
+    camera_fb_t *photo = esp_camera_fb_get();
+    gpio_set_level(4, 0);
+    if (!photo) {
+        ESP_LOGE(DEVICE, "[CAM] Photo capture failed");
+        httpd_resp_send_500(req);
+        return ESP_FAIL;
+    }
+    
+    ESP_LOGI(DEVICE, "[CAM] Sending photo");
+    esp_err_t res = httpd_resp_set_type(req, "image/jpeg");
+    if(res == ESP_OK){
+        res = httpd_resp_set_hdr(req, "Content-Disposition", "inline; filename=capture.jpg");
+    }
+
+    if(res == ESP_OK){
+        if(photo->format == PIXFORMAT_JPEG){
+            res = httpd_resp_send(req, (const char *)photo->buf, photo->len);
+        } else {
+            jpg_chunking_t jchunk = {req, 0};
+            res = frame2jpg_cb(photo, 80, jpg_encode_stream, &jchunk)?ESP_OK:ESP_FAIL;
+            httpd_resp_send_chunk(req, NULL, 0);
+        }
+    }
+    
+    taken_photo_len = photo->len;
+    taken_photo_width = photo->width;
+    taken_photo_height = photo->height;
+
+    ESP_LOGI(DEVICE, "[CAM] Photo taken! Its size was: %zu bytes\n", photo->len);
+    esp_camera_fb_return(photo);
+    return ESP_OK;
+}
 
 /**
  * @brief Webserver Start
@@ -330,9 +343,38 @@ httpd_handle_t start_webserver(void) {
         ESP_LOGI(DEVICE, "[HTTP] Server start success");
         ESP_LOGI(DEVICE, "[HTTP] Registering endpoints");
         // Register availiable paths and requests
-        httpd_register_uri_handler(server, &uri_get);
-        httpd_register_uri_handler(server, &img_get);
-        httpd_register_uri_handler(server, &img_post);
+        // MAIN PAGE - INDEX.HTML
+        httpd_uri_t index_get = {
+            .uri      = "/",
+            .method   = HTTP_GET,
+            .handler  = get_handler,
+            .user_ctx = NULL
+        };
+        // GET LATEST PHOTO
+        httpd_register_uri_handler(server, &index_get);
+        httpd_uri_t latest_get = {
+            .uri      = "/latest-photo.jpg",
+            .method   = HTTP_GET,
+            .handler  = img_handler,
+            .user_ctx = NULL
+        };
+        httpd_register_uri_handler(server, &latest_get);
+        // PIR EVENT - TAKE PHOTO
+        httpd_uri_t pir_post = {
+            .uri      = "/pir",
+            .method   = HTTP_GET,
+            .handler  = pir_handler,
+            .user_ctx = NULL
+        };
+        httpd_register_uri_handler(server, &pir_post);
+        // MANUAL REQUEST FOR PHOTO (NOT SAVED)
+        httpd_uri_t take_post = {
+            .uri      = "/take-photo",
+            .method   = HTTP_GET,
+            .handler  = take_handler,
+            .user_ctx = NULL
+        };
+        httpd_register_uri_handler(server, &take_post);
     }
 
     return server;
@@ -346,49 +388,6 @@ void stop_webserver(httpd_handle_t server) {
     if (server) {
         httpd_stop(server);
     }
-}
-// ================================================================
-
-// ============================ SPIFFS ============================
-/**
- * @brief Initialize spiffs
- * source: https://github.com/espressif/esp-idf/blob/master/examples/storage/spiffs/main/spiffs_example_main.c
- */
-esp_vfs_spiffs_conf_t init_spiffs() {
-    esp_vfs_spiffs_conf_t configuration = {
-        .base_path = MOUNTPOINT,
-        .partition_label = NULL,
-        .max_files = 5,
-        .format_if_mount_failed = false
-    };
-
-    esp_err_t ret = esp_vfs_spiffs_register(&configuration);
-
-    if (ret != ESP_OK) {
-        if (ret == ESP_FAIL) {
-            ESP_LOGE(DEVICE, "[SPIFFS] Failed to mount or format filesystem");
-        } else if (ret == ESP_ERR_NOT_FOUND) {
-            ESP_LOGE(DEVICE, "[SPIFFS] Failed to find SPIFFS partition");
-        } else {
-            ESP_LOGE(DEVICE, "[SPIFFS] Failed to initialize SPIFFS (%s)", esp_err_to_name(ret));
-        }
-        return;
-    }
-
-    size_t total = 0, used = 0;
-    ret = esp_spiffs_info(configuration.partition_label, &total, &used);
-    if (ret != ESP_OK) {
-        ESP_LOGE(DEVICE, "[SPIFFS] Failed to get SPIFFS partition information (%s)", esp_err_to_name(ret));
-    } else {
-        ESP_LOGI(DEVICE, "[SPIFFS] Partition size: total: %d, used: %d", total, used);
-    }
-
-    return configuration;
-}
-
-void deinit_spiffs(esp_vfs_spiffs_conf_t *configuration) {
-    esp_vfs_spiffs_unregister(configuration->partition_label);
-    ESP_LOGI(DEVICE, "[SPIFFS] SPIFFS unmounted");
 }
 // ================================================================
 
@@ -411,9 +410,7 @@ esp_err_t init_camera()
 }
 
 /**
- * @brief Takes a picture and saves it to the SD card
- * 
- * @return esp_err_t 
+ * @brief Takes a picture and saves it to temp memory
  */
 esp_err_t take_picture() {
     ESP_LOGI(DEVICE, "[CAM] Taking photo");
@@ -422,23 +419,17 @@ esp_err_t take_picture() {
     vTaskDelay(delay);
     camera_fb_t *photo = esp_camera_fb_get();
     gpio_set_level(4, 0);
-    if (!photo) {
-        ESP_LOGE(DEVICE, "[CAM] Photo capture failed");
-        return ESP_FAIL;
-    }
-    
-    ESP_LOGI(DEVICE, "[CAM] Saving photo to SPIFFS");
 
-    FILE *file = fopen("/spiffs/latest-photo.jpg", "w");
-    if (file != NULL) {
-        ESP_LOGI(DEVICE, "[CAM] Photo saved");
-        fwrite(photo->buf, 1, photo->len, file);
-    } else {
-        ESP_LOGE(DEVICE, "[CAM] Saving of photo (1) to SPIFFS failed");
-        esp_camera_fb_return(photo);
-        return ESP_FAIL;
+    if (taken_photo_buf != NULL) {
+        free(taken_photo_buf);
     }
-    fclose(file);
+    taken_photo_buf = malloc(photo->len);
+    mempcpy(taken_photo_buf, photo->buf, photo->len);
+
+    taken_photo_format = photo->format;
+    taken_photo_len = photo->len;
+    taken_photo_width = photo->width;
+    taken_photo_height = photo->height;
 
     ESP_LOGI(DEVICE, "[CAM] Photo taken! Its size was: %zu bytes\n", photo->len);
     esp_camera_fb_return(photo);
@@ -461,8 +452,6 @@ void app_main() {
     wifi_config_ap();
     // Start Webserver
     httpd_handle_t server = start_webserver();
-    // Initialize sdcard
-    esp_vfs_spiffs_conf_t config = init_spiffs();
     // Initialize camera
     if (ESP_OK != init_camera()) {
         return;
@@ -478,7 +467,6 @@ void app_main() {
     while(1) {}
     // Unreachable due to infinite loop
     stop_webserver(server);
-    deinit_spiffs(&config);
     // ================================================================
 }
 // ================================================================
