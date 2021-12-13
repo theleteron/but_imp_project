7adc23c2090a42f34707ab18464f3ddd6879aeca
Roman Janiczek <roman@janiczek.cz>
2021-12-13 07:05:12 +0100

    Updated README.md & added data printout for PIR HTTP

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 # IMP - Security device - Motion detection & camera
-This project was focuse on creating motion detection system that will take a picture after motion has been detected.  
-Motion detection part of the project is using `WeMos D1 R32 UNO ESP32` and `AM312 PIR Motion sensor`.  
-This is connected using breadboard to the `AI-Thinker's ESP32-CAM`.
-## Connection schema
-![Connection schema](assests/circuit.svg)
+The focus of this project is the creation of a motion detection system that will take a picture after motion is detected.  
+The motion detection part of the project uses `WeMos D1 R32 UNO ESP32` and `AM312 PIR Motion sensor`. This part connects to the wireless network created by `AI-Thinker's ESP32-CAM`, allowing it to pass detected motion information using HTTP requests.  
+The latest captured picture shows on the webserver available on `AI-Thinker's ESP32-CAM`.
 ## Usage
+The project uses [PlatformIO in VSCode](https://docs.platformio.org/en/latest/integration/ide/vscode.html); as such, the simplest way to use this project would be to open it in PlatformIO and upload it to your device.  
+Please note that this project has two parts that you must upload separately - `security-pir` (`WeMos D1 R32 UNO ESP32`) and `security-cam` (`AI-Thinker's ESP32-CAM`).  
+For a detailed tutorial on opening a project in PlatformIO in VSCode, please refer to their documentation.
+## Connection schema
+The connection contains a PIR sensor connected to `WeMos D1 R32 UNO ESP32` and one `debug` LED that will blink when motion is detected.  
+Communication between `WeMos D1 R32 UNO ESP32` and `AI-Thinker's ESP32-CAM` is purely wireless.  
+![Connection schema](assests/circuit.png)
+## Known limitations & bugs
+- ...
--- security-pir/src/main.c
+++ security-pir/src/main.c
@@ -41,7 +41,7 @@ esp_ip4_addr_t gateway;
 // ================================================================
 
 
-const TickType_t delay = 250 / portTICK_PERIOD_MS;              // 0.25s
+const TickType_t delay = 1000 / portTICK_PERIOD_MS;              // 1s
 
 
 // ============================= WIFI =============================
@@ -114,6 +114,8 @@ void wifi_config_sta(void) {
  * source: https://github.com/espressif/esp-idf/blob/5c33570524118873f7bd32490c7a0442fede4bf8/examples/protocols/esp_http_client/main/esp_http_client_example.c
  */
 esp_err_t http_event_handler(esp_http_client_event_t *evt) {
+    static char *output_buffer;  // Buffer to store response of http request from event handler
+    static int output_len;       // Stores number of bytes read
     switch(evt->event_id) {
         case HTTP_EVENT_ERROR:
             ESP_LOGD(DEVICE, "[HTTP] ERROR");
@@ -129,12 +131,47 @@ esp_err_t http_event_handler(esp_http_client_event_t *evt) {
             break;
         case HTTP_EVENT_ON_DATA:
             ESP_LOGD(DEVICE, "[HTTP] DATA {len=%d}", evt->data_len);
+            if (!esp_http_client_is_chunked_response(evt->client)) {
+                // If user_data buffer is configured, copy the response into the buffer
+                if (evt->user_data) {
+                    memcpy(evt->user_data + output_len, evt->data, evt->data_len);
+                } else {
+                    if (output_buffer == NULL) {
+                        output_buffer = (char *) malloc(esp_http_client_get_content_length(evt->client));
+                        output_len = 0;
+                        if (output_buffer == NULL) {
+                            ESP_LOGE(DEVICE, "[HTTP] Failed to allocate memory for output buffer");
+                            return ESP_FAIL;
+                        }
+                    }
+                    memcpy(output_buffer + output_len, evt->data, evt->data_len);
+                }
+                output_len += evt->data_len;
+            }
             break;
         case HTTP_EVENT_ON_FINISH:
             ESP_LOGD(DEVICE, "[HTTP] FINISHED");
+            if (output_buffer != NULL) {
+                // Response is accumulated in output_buffer. Uncomment the below line to print the accumulated response
+                ESP_LOG_BUFFER_CHAR(DEVICE, output_buffer, output_len);
+                free(output_buffer);
+                output_buffer = NULL;
+            }
+            output_len = 0;
             break;
         case HTTP_EVENT_DISCONNECTED:
             ESP_LOGI(DEVICE, "[HTTP] DISCONNECTED");
+            int mbedtls_err = 0;
+            esp_err_t err = esp_tls_get_and_clear_last_error(evt->data, &mbedtls_err, NULL);
+            if (err != 0) {
+                if (output_buffer != NULL) {
+                    free(output_buffer);
+                    output_buffer = NULL;
+                }
+                output_len = 0;
+                ESP_LOGI(DEVICE, "[HTTP] Last esp error code: 0x%x", err);
+                ESP_LOGI(DEVICE, "[HTTP] Last mbedtls failure: 0x%x", mbedtls_err);
+            }
             break;
     }
     return ESP_OK;
@@ -169,6 +206,8 @@ void send_request_to_camera() {
         ESP_LOGE(DEVICE, "[HTTP] Sending PIR signal to CAM failed {%s}", 
                 esp_err_to_name(err));
     }
+    
+    ESP_LOG_BUFFER_CHAR(DEVICE, local_response_buffer, strlen(local_response_buffer));
 
     esp_http_client_cleanup(client);
 }
